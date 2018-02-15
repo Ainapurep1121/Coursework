587bbde8ab502207278c62bcf2033f3e8bcf195e
alixramos <alixramos17@gmail.com>
2018-02-15 13:44:40 -0600

    Add files via upload

--- Lab5_TestBench.sv
+++ Lab5_TestBench.sv
@@ -22,6 +22,10 @@ logic [6:0] AhexL,
 logic [7:0] ans_A, ans_B;
 logic			ans_X;
 
+// A counter to count the instances where simulation results
+// do no match with expected results
+integer ErrorCnt = 0;
+
 // Instantiating the DUT
 // Make sure the module and signal names match with those in your design
 Multiplier multiplier0(.*);	
@@ -29,17 +33,17 @@ Multiplier multiplier0(.*);
 //Toggle clock
 //#1 means wait for a delay of 1 timeunit
 always begin : CLOCK_GENERATION
-	#1 Clk = ~Clk;
+#1 Clk = ~Clk;
 end
 
-intitial begin: CLOCK_INITIALIZATION
+initial begin: CLOCK_INITIALIZATION
 	Clk = 0;
 end
 
 //Testing begins here
 //Initial block is not synthesizable
 
-intial begin: TEST_VECTORS
+initial begin: TEST_VECTORS
 
 //Declare the solutions for comparison 
 ans_A = 8'h63; 
@@ -47,18 +51,18 @@ ans_B = 8'hFE;
 ans_X = 1'b1;
 
 //Actual Simulation Test
-ClrA_LdB 	= 0;    	 // Set to 0, before toggling
+ClearA_LoadB 	= 0;    	 // Set to 0, before toggling
 S 				= 8'hC5;	 // Hex -59 = 1100 0101 -< Check
-ClrA_LdB 	= 1;	    //Toggle ClrA_LdB
-ClrA_LdB 	= 0;       
+ClearA_LoadB 	= 1;	    //Toggle ClrA_LdB
+ClearA_LoadB 	= 0;       
 S 				= 8'h07;  //Hex 07 = 0000 0111 -< check 
 Run 			= 1;
 Run 			= 0;
 
 //Store Results to check if computation worked. 
 //This chunk can be moved around!
-Aval = blah
-Bval = blah 
+//Aval = blah
+//Bval = blah 
 
 //Repeat Calculation Check
 #2 S 				= 9'h02;
@@ -69,12 +73,12 @@ Bval = blah
 #5 Reset 	= 1;
 	Reset 	= 0;
 	S 			= 8'hC5;
-	ClrA_LdB = 1;
-	ClrA_LdB = 0;
+	ClearA_LoadB = 1;
+	ClearA_LoadB = 0;
 	S 			= 8'h07;
 #2 Run 		= 1;
 #2 Run 		= 0;
-end
+
 
 //Check if the compuation units worked as expected
 
@@ -82,13 +86,13 @@ end
 			ErrorCnt++;
 	 if(Bval != ans_B)
 			ErrorCnt++;
-	 if(Xval != ans_X)
+	 if(X != ans_X)
 			ErrorCnt++;
 	 
 if(ErrorCnt == 0)
 	$display("Success!"); 
 else
 	$display("%d error(s) detected.", ErrorCnt);
-end
 
+end
 endmodule
