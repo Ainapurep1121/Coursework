c2883b7b5ce03fd25e78cad3480ec7ebe97ba6e0
alixramos <alixramos17@gmail.com>
2018-02-15 11:41:42 -0600

    Add files via upload

---
+++ Multiplier.sv
@@ -0,0 +1,122 @@
+module Multiplier
+(
+
+    input   logic           Clk,        // 50MHz clock is only used to get timing estimate data
+    input   logic           Reset,      // From push-button 0.  Remember the button is active low (0 when pressed)
+    input   logic           Run,        // From push-button 3.
+    input   logic           ClearA_LoadB,         // From slider switches
+	 input 	logic	[7:0]		 S,
+	 
+	 output  logic [6:0]  	 AhexL,				// Hex drivers display both inputs to the adder.
+									 AhexU,
+									 BhexL,
+									 BhexU,
+	 output  logic [7:0] 	 Aval, 
+	   							 Bval,
+	 output  logic 			 X);
+
+// Need module that describes control 
+    logic          Clr_A;      // From push-button 1
+	 logic          Shift;      // From push-button 1
+	 logic          Add;      // From push-button 1
+	 logic          Sub;      // From push-button 1
+	 logic[7:0]     A;  // Shift register A
+	 logic[7:0]     B;  // Shift register B
+	 logic[8:0]     Aout;  // Shift register A
+	 logic 			 Xout;  // Sign extension bit 
+	 logic 			 LSB;
+	 logic[6:0]      AhexU_comb;
+    logic[6:0]      AhexL_comb;
+    logic[6:0]      BhexU_comb;
+    logic[6:0]      BhexL_comb;
+	 
+	 assign LSB = B[0];
+	 
+	 
+	ninebit_adder 		addition_unit (
+							.A(A),
+							.S(S^Sub),
+							.M(Sub),
+							.AxS(Aout),
+							.X(Xout));
+	reg_8					register_unitA  ( 		// come back to these imputs
+							.Clk(Clk), 
+							.Reset(Reset), 
+							.Shift_In(A[7]), 
+							.Load(ClearA_LoadB), 
+							.Shift_En(Shift),
+							.D(S));
+	reg_8					register_unitB  (
+							.Clk(Clk), 
+							.Reset(Reset), 
+							.Shift_In(B[7]), 
+							.Load(ClearA_LoadB), 
+							.Shift_En(Shift),
+							.D(S));
+	control_logic		control_unit (
+							.Add(Add),
+							.Sub(Sub),
+							.Shift(Shift),
+							.Clr_A(Clr_A)
+							);
+	 
+	 always_ff @(posedge Clk) begin
+        
+        if (Clr_A) begin
+            // if clear var is high, clear A
+            A <= 8'h000;
+            X <= 1'b0;
+        end else if (!Reset) begin
+            // if reset is pressed, clear the adder's input registers
+				A <= 8'h000;
+				B <= 8'h000;
+            X <= 1'b0;
+        end else if (!ClearA_LoadB) begin
+            // if reset is pressed, clear the adder's input registers
+            A <= 8'h000;
+            B <= S;
+            X <= 1'b0;
+        end else if (Add && LSB) begin
+				A <= Aout[7:0]; 
+				X <= Xout;
+		  end else if (Shift) begin
+				B = B	>> 1;
+				B[7] = A[0];
+				A = A >>> 1;
+				A[7] = X;
+				Aval = A;
+				Bval = B;
+		  end 
+	 end
+	 
+	 always_ff @(posedge Clk) begin
+        
+        AhexU <= AhexU_comb;
+        AhexL <= AhexL_comb;
+        BhexU <= BhexU_comb;
+        BhexL <= BhexL_comb;
+        
+    end
+	 
+	 HexDriver        HexAL (
+                        .In0(A[3:0]),
+                        .Out0(AhexL_comb) );
+	 HexDriver        HexBL (
+                        .In0(B[3:0]),
+                        .Out0(BhexL_comb) );
+								
+	 //When you extend to 8-bits, you will need more HEX drivers to view upper nibble of registers, for now set to 0
+	 HexDriver        HexAU (
+                        .In0(A[7:4]),
+                        .Out0(AhexU_comb) );	
+	 HexDriver        HexBU (
+                        .In0(B[7:4]),
+                        .Out0(BhexU_comb) );
+
+//	  sync button_sync[3:0] (Clk, {~Reset, ~Run, ClearA_LoadB}, {Reset_SH, Run_SH, LoadB_SH, Execute_SH});
+//	  sync Din_sync[7:0] (Clk, Din, Din_S);
+//	  sync F_sync[2:0] (Clk, F, F_S);
+//	  sync R_sync[1:0] (Clk, R, R_S);
+	
+
+endmodule
---
+++ control_logic.sv
@@ -0,0 +1,110 @@
+module control_logic (input  logic Clk, Reset, LoadA, LoadB, Run, //change
+                output logic Add, Sub, Shift, Clr_A );
+
+					 //make current state an output
+					 
+    // Declare signals curr_state, next_state of type enum
+    // with enum values of A, B, ..., F as the state values
+	 // Note that the length implies a max of 8 states, so you will need to bump this up for 8-bits
+    enum logic [4:0] {Start, ClearA, Add1, Shift1, Add2, Shift2,
+							 Add3, Shift3, Add4, Shift4, Add5, Shift5, 
+							 Add6, Shift6, Add7, Shift7, Sub8, Shift8, Done}   curr_state, next_state; 
+	
+	//updates flip flop, current state is the only one
+    always_ff @ (posedge Clk)  
+    begin
+        if (Reset)
+            curr_state <= Start;
+        else 
+            curr_state <= next_state;
+    end
+
+    // Assign outputs based on state
+	always_comb
+    begin
+		  Add = 0;
+        Sub = 0;
+		  Clr_A = 0;
+		  Shift = 0;
+		  next_state  = curr_state;	//required because I haven't enumerated all possibilities below
+        unique case (curr_state) 
+
+				
+				Start: if (Run)
+                       next_state = ClearA;
+				ClearA: begin  
+							  next_state = Add1;
+							  Clr_A = 1;
+						  end
+				Add1: begin 
+							  next_state = Shift1;
+							  Add = 1;
+						end
+				Shift1: begin    
+							  next_state = Add2;
+							  Shift = 1;
+						  end  
+				Add2: begin
+							  next_state = Shift2;
+							  Add = 1;
+						end
+				Shift2: begin    
+							  next_state = Add3;
+							  Shift = 1;
+						  end  
+				Add3: begin
+							  next_state = Shift3;
+							  Add = 1;
+						end
+				Shift3: begin    
+							  next_state = Add4;
+							  Shift = 1;
+						  end  
+				Add4: begin
+							  next_state = Shift4;
+							  Add = 1;
+						end
+				Shift4: begin    
+							  next_state = Add5;
+							  Shift = 1;
+						  end  
+				Add5: begin
+							  next_state = Shift5;
+							  Add = 1;
+						end
+				Shift5: begin    
+							  next_state = Add6;
+							  Shift = 1;
+						  end  
+				Add6: begin
+							  next_state = Shift6;
+							  Add = 1;
+						end
+				Shift6: begin    
+							  next_state = Add7;
+							  Shift = 1;
+						  end  
+				Add7: begin
+							  next_state = Shift7;
+							  Add = 1;
+						end
+				Shift7: begin    
+							  next_state = Sub8;
+							  Shift = 1;
+						  end  
+				Sub8: begin
+							  next_state = Shift8;
+							  Add = 1;
+							  Sub = 1;
+						end
+				Shift8: begin    
+							  next_state = Done;
+							  Shift = 1;
+						  end  
+				Done:      if (~Run) 
+                       next_state = Start;
+							  
+        endcase
+    end
+
+endmodule
