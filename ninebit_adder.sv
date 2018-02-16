ce07724f9c0f56fc8a2c60293558495400c7fa29
alixramos <alixramos17@gmail.com>
2018-02-15 22:44:20 -0600

    Add files via upload

--- ninebit_adder.sv
+++ ninebit_adder.sv
@@ -8,11 +8,20 @@ module ninebit_adder
 );
 
 	logic C0, C1;
+	logic [7:0] SubV;
+	assign SubV[0] = S[0] ^ M;
+	assign SubV[1] = S[1] ^ M;
+	assign SubV[2] = S[2] ^ M;
+	assign SubV[3] = S[3] ^ M;
+	assign SubV[4] = S[4] ^ M;
+	assign SubV[5] = S[5] ^ M;
+	assign SubV[6] = S[6] ^ M;
+	assign SubV[7] = S[7] ^ M;
 	  
-	four_bit_ra FRA0(.x(A[3 : 0]), .y(S[3 : 0]), .cin( M), .s(AxS[3 : 0]), .cout(C0));
-	four_bit_ra FRA1(.x(A[7 : 4]), .y(S[7 : 4]), .cin(C0), .s(AxS[7 : 4]), .cout(C1));
+	four_bit_ra FRA0(.x(A[3 : 0]), .y(SubV[3 : 0]), .cin( M), .s(AxS[3 : 0]), .cout(C0));
+	four_bit_ra FRA1(.x(A[7 : 4]), .y(SubV[7 : 4]), .cin(C0), .s(AxS[7 : 4]), .cout(C1));
 	
-	assign X = (C1 | A[7]);
+	assign X = (C1 | AxS[7]);
 	
 	
 endmodule
